e1bfa21cd66e95b33af6e68b043d276e984a79b5
vivek <vivekbilla822@gmail.com>
2026-05-20 18:19:01 +0530

    changes

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth"
+const apiUrl = import.meta.env.VITE_API_URL|| "http://localhost:5000/api/auth"
+
 
 const API = axios.create({
   baseURL: apiUrl
@@ -8,6 +9,5 @@ const API = axios.create({
 
 export const loginUser = (data) => API.post("/login", data)
 export const registerUser = (data) => API.post("/register", data)
-
 
 export default API

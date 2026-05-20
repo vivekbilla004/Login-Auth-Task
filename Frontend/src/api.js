1366e96981d05008c961cff79eb23f9b4d511a71
vivek <vivekbilla822@gmail.com>
2026-05-20 18:38:56 +0530

    changes

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
-const apiUrl = "https://login-auth-task.onrender.com/api/auth"
+const apiUrl = "https://login-auth-task.onrender.com/"
 
 const API = axios.create({
   baseURL: apiUrl
 })
 
-export const loginUser = (data) => API.post("/login", data)
-export const registerUser = (data) => API.post("/register", data)
+export const loginUser = (data) => API.post("api/auth/login", data)
+export const registerUser = (data) => API.post("api/auth/register", data)
 
 export default API
